df19bc3437be383096e4a5841aa298433aca89f2
Pat Lown <lown@wisc.edu>
2019-01-21 13:59:22 -0600

    inorderSuccessor

--- Tree/285. Inorder Successor in BST/Solution.java
+++ Tree/285. Inorder Successor in BST/Solution.java
@@ -5,6 +5,20 @@
     *     TreeNode(int x) { val = x; }
     * }
     */
+
+    /*
+    Idea: Keep track of successors in a stack, every time you are about to move left in the BST,
+    store the current node in the stack.  Once you find your element, return the top node in the
+    stack.
+
+     ^This approach is okay but needs some work and fails some test cases.  First, we don't actually need
+     a stack because we only need the last successor that we saw, not the previous ones.  Thus, every time
+     we go left, we can save the current node before we move to its left.  Keeping track of this
+     node is not all we have to do.  When we reach the target node, we have two choices: its successor is
+     the one we've stored through our traversal or is the farthest left value in the right subtree. Therefore, we 
+     must check if we have a right subtree before returning the successor.  The edge cases for this
+     algorithm occur when we have 0 or 1 nodes, in either case return null cause there can be no successor.
+    */
    class Solution {
        public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
            //edge cases, 0 or 1 nodes, return null
