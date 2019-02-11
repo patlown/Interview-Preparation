a3acd6d68392c28bc509e56cc3cea70138b46012
Pat Lown <lown@wisc.edu>
2019-02-11 15:17:31 -0600

    compareVersion

---
+++ Arrays and Strings/165. Compare Version Numbers/Solution.java
@@ -0,0 +1,26 @@
+/*
+    Pretty Straightforward: split the string by the . character and then
+    compare each element in the array.  iterate through the size of the longer
+    array by checking if the shorter array still has elements, if it doesn't
+    consider them as zero and keep comparing. 
+*/
+
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+     String[] levels1 = version1.split("\\.");
+     String[] levels2 = version2.split("\\.");
+     
+     int length = Math.max(levels1.length, levels2.length);
+     for (int i=0; i<length; i++) {
+         Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+         Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+         int compare = v1.compareTo(v2);
+         if (compare != 0) {
+             return compare;
+         }
+     }
+     
+     return 0;
+     }
+ }
